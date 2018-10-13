1d98d046fa7e206b7201645658a0f8b926f25427
straydragon <straydragonl@foxmail.com>
2018-10-13 14:47:14 +0800

    Add exception header files

---
+++ ADTs/exceptions/PreconditionFailedException.h
@@ -0,0 +1,10 @@
+#ifndef DATASTRUCTURE_ALGORITHM_CPP_PRECONDITIONFAILED
+#define DATASTRUCTURE_ALGORITHM_CPP_PRECONDITIONFAILED
+#include "Exception.h"
+class PreconditionFailedException : public Exception {
+ public:
+  explicit PreconditionFailedException(const std::string& msg = "")
+      : Exception("(Assert precondition failed!)" + msg) {}
+};
+
+#endif
