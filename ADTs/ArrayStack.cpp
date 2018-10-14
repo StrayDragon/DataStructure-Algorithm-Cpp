d109b053d4a8f7eb273bde599daf191c28b1f7b2
straydragon <straydragonl@foxmail.com>
2018-10-14 20:56:52 +0800

    add ArrayStack<E> and LinkedStack<E> definitions

--- ADTs/ArrayStack.cpp
+++ ADTs/ArrayStack.cpp
@@ -4,27 +4,38 @@
 
 #include "ArrayStack.h"
 
-template<typename E>
-ArrayStack<E>::ArrayStack() {
+template <typename E>
+ArrayStack<E>::ArrayStack() : _top(-1) {}
 
-}
-
-template<typename E>
+template <typename E>
 bool ArrayStack<E>::isEmpty() const {
-	return false;
+  return _top < 0;
 }
 
-template<typename E>
-bool ArrayStack<E>::push(const E &e) {
-	return false;
+template <typename E>
+bool ArrayStack<E>::push(const E &element) {
+  bool canPushed = false;
+  if (_top < MAX_STACK_CAPACITY - 1) {
+    _top++;
+    _elements[_top] = element;
+    canPushed = true;
+  }
+  return canPushed;
 }
 
-template<typename E>
+template <typename E>
 bool ArrayStack<E>::pop() {
-	return false;
+  bool wasPopped = false;
+  if (!isEmpty()) {
+    _top--;
+    wasPopped = true;
+  }
+  return wasPopped;
 }
 
-template<typename E>
+template <typename E>
 E ArrayStack<E>::top() const {
-	return nullptr;
+  assert(!isEmpty());
+
+  return _elements[_top];
 }
