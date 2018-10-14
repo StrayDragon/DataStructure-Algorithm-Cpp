4c15f4b4f6e3016a85d01dec7d9178c50fec277c
straydragon <straydragonl@foxmail.com>
2018-10-14 20:55:15 +0800

    add ArrayStack<E> and LinkedStack<E> test file

--- Test/unit_test_main.cpp
+++ Test/unit_test_main.cpp
@@ -5,19 +5,19 @@
 #include <cassert>
 #include <iostream>
 #include <vector>
+using namespace std;
+#include "../ADTs/exceptions/PreconditionFailedException.h"
 
 #include "../ADTs/ArrayList.h"
-#include "../ADTs/LinkedList.cpp"
-#include "../ADTs/exceptions/PreconditionFailedException.h"
+#include "../ADTs/LinkedList.h"
 #include "../ADTs/interfaces/List.h"
-using namespace std;
 
 template <typename E>
 void ADTListTestMachine(List<E>* list) {
   cout << flush << "\ttest target implementation : " << typeid(*list).name();
   if (typeid(*list) == typeid(LinkedList<E>)) {
     LinkedList<E> linkedListCopy;
-    list = new LinkedList<E>(linkedListCopy);
+    list = new LinkedList<E>(linkedListCopy);  // TODO:浅拷贝
   }
   assert(list->getLength() == 0 && list->isEmpty());
 
@@ -56,8 +56,43 @@ void ADTListTestMachine(List<E>* list) {
   cout << "\t\t==OK==" << endl;
 }
 
+#include "../ADTs/ArrayStack.h"
+#include "../ADTs/LinkedStack.h"
+#include "../ADTs/interfaces/Stack.h"
+
+template <typename E>
+void ADTStackTestMachine(Stack<E>* stack) {
+  cout << flush << "\ttest target implementation : " << typeid(*stack).name();
+
+  if (typeid(*stack) == typeid(LinkedStack<E>)) {
+    LinkedStack<E> linkedStackCopy;
+    assert(linkedStackCopy.push(1) == true);
+
+    stack = new LinkedStack<E>(linkedStackCopy);
+    assert(stack->pop() == true);
+  }
+
+  assert(stack->isEmpty() == true);
+
+  for (int i = 0; i < MAX_STACK_CAPACITY; i++) {
+    assert(stack->push(i) == true);
+  }
+  if (typeid(*stack) == typeid(ArrayStack<E>)) {
+    assert(stack->push(-1) == false);
+  }
+
+  assert(stack->top() == MAX_STACK_CAPACITY - 1);
+
+  for (int i = 0; i < MAX_STACK_CAPACITY; i++) {
+    assert(stack->pop() == true);
+  }
+  assert(stack->pop() == false);
+
+  cout << "\t\t==OK==" << endl;
+}
+
 int main() {
-  cout << "Hello! This is unit test main." << endl;
+  cout << "Hello! This is unit test main.\n" << endl;
 
   auto* arrayList = new ArrayList<int>();
   ADTListTestMachine(arrayList);
@@ -65,7 +100,12 @@ int main() {
   auto* linkedList = new LinkedList<int>();
   ADTListTestMachine(linkedList);
 
-  cout << "\nCONGRATULATIONS! ALL TESTS PASSED SUCCESSFULLY " << endl;
+  auto* arrayStack = new ArrayStack<int>();
+  ADTStackTestMachine(arrayStack);
 
+  auto* linkedStack = new LinkedStack<int>();
+  ADTStackTestMachine(linkedStack);
+
+  cout << "\nCONGRATULATIONS! ALL TESTS PASSED SUCCESSFULLY " << endl;
   return 0;
 }
