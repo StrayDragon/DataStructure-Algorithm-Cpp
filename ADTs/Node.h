6fca56b35f795057266db1eb2d5a97a18aea1342
Peilong Li <straydragonl@foxmail.com>
2018-10-14 22:46:15 +0800

    simplify the class Node<E>, remove .cpp file mixin in .h

--- ADTs/Node.h
+++ ADTs/Node.h
@@ -31,6 +31,34 @@ class Node {
 
   Node<E>* getNext() const;
 };
+template <typename E>
+Node<E>::Node() : _next(nullptr) {}
 
-#include "Node.cpp"
+template <typename E>
+Node<E>::Node(const E& element) : _element(element), _next(nullptr) {}
+
+template <typename E>
+Node<E>::Node(const E& element, Node<E>* next)
+				: _element(element), _next(next) {}
+
+template <typename E>
+void Node<E>::setElement(const E& element) {
+	_element = element;
+}
+
+template <typename E>
+void Node<E>::setNext(Node<E>* next) {
+	_next = next;
+}
+
+template <typename E>
+E Node<E>::getElement() const {
+	return _element;
+}
+
+template <typename E>
+Node<E>* Node<E>::getNext() const {
+	return _next;
+}
+
 #endif  // DATASTRUCTURE_ALGORITHM_CPP_NODE_H
