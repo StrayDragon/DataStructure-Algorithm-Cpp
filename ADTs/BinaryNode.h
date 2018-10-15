320dfed69102b39d73952e98906c22b4aff27685
Peilong Li <straydragonl@foxmail.com>
2018-10-15 21:03:29 +0800

    simplify the POCO BinaryNode<E>

--- ADTs/BinaryNode.h
+++ ADTs/BinaryNode.h
@@ -15,59 +15,38 @@ private:
 public:
 		BinaryNode():_left(nullptr),_right(nullptr){}
 
-		BinaryNode(E _element) : _element(_element) ,_left(nullptr),_right(nullptr){}
+		BinaryNode(const E& element) : _element(element) ,_left(nullptr),_right(nullptr){}
 
-		BinaryNode(E _element, BinaryNode<E> *_left, BinaryNode<E> *_right) : _element(_element), _left(_left),
-		                                                                      _right(_right) {}
-		bool isLeaf() const;
+		BinaryNode(const E& element, BinaryNode<E>* left, BinaryNode<E>* right) : _element(element), _left(left),
+		                                                                      _right(right) {}
+		bool isLeaf() const{
+			return (_left == nullptr && _right == nullptr);
+		}
 
-		E getElement() const;
+		E getElement() const{
+			return _element;
+		}
 
-		void setElement(E _element);
+		void setElement(E element){
+			_element = element;
+		}
 
-		BinaryNode<E> *getLeft() const;
+		BinaryNode<E> *getLeft() const{
+			return _left;
+		}
 
-		void setLeft(BinaryNode<E> *_left);
+		void setLeft(BinaryNode<E> *left){
+			_left = left;
+		}
 
-		BinaryNode<E> *getRight() const;
+		BinaryNode<E> *getRight() const{
+			return _right;
+		}
 
-		void setRight(BinaryNode<E> *_right);
+		void setRight(BinaryNode<E> *right){
+			_right = right;
+		}
 };
-
-template<typename E>
-bool BinaryNode<E>::isLeaf() const {
-	return false;
-}
-
-template<typename E>
-E BinaryNode<E>::getElement() const {
-	return nullptr;
-}
-
-template<typename E>
-void BinaryNode<E>::setElement(E _element) {
-
-}
-
-template<typename E>
-void BinaryNode<E>::setLeft(BinaryNode<E> *_left) {
-
-}
-
-template<typename E>
-BinaryNode<E> *BinaryNode<E>::getLeft() const {
-	return nullptr;
-}
-
-template<typename E>
-BinaryNode<E> *BinaryNode<E>::getRight() const {
-	return nullptr;
-}
-
-template<typename E>
-void BinaryNode<E>::setRight(BinaryNode<E> *_right) {
-
-}
 
 
 #endif //DATASTRUCTURE_ALGORITHM_CPP_BINARYNODE_H
