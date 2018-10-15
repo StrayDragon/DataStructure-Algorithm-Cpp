acf13e1b184ce527b0afb9e49c76a67fa76ff301
straydragon <straydragonl@foxmail.com>
2018-10-15 09:30:21 +0800

    finish ArrayQueue<E> difinition

--- ADTs/ArrayQueue.cpp
+++ ADTs/ArrayQueue.cpp
@@ -4,22 +4,45 @@
 
 #include "ArrayQueue.h"
 
-template<typename E>
+template <typename E>
+ArrayQueue<E>::ArrayQueue()
+    : _head(0), _rear(MAX_QUEUE_CAPACITY - 1), _count(0) {}
+
+template <typename E>
 bool ArrayQueue<E>::isEmpty() const {
-	return false;
+  return _count == 0;
 }
 
-template<typename E>
+template <typename E>
 bool ArrayQueue<E>::enqueue(const E &element) {
-	return false;
+  bool canEnqueue = false;
+
+  if (_count < MAX_QUEUE_CAPACITY) {
+    _rear = (_rear + 1) % MAX_QUEUE_CAPACITY;
+    _elements[_rear] = element;
+    _count++;
+    canEnqueue = true;
+  }
+
+  return canEnqueue;
 }
 
-template<typename E>
+template <typename E>
 bool ArrayQueue<E>::dequeue() {
-	return false;
+  bool canDequeue = false;
+
+  if (!isEmpty()) {
+    _head = (_head + 1) % MAX_QUEUE_CAPACITY;
+    _count--;
+    canDequeue = true;
+  }
+
+  return canDequeue;
 }
 
-template<typename E>
-E ArrayQueue<E>::front() const {
-	return nullptr;
+template <typename E>
+E ArrayQueue<E>::front() const noexcept(false) {
+  if (isEmpty()) throw PreconditionFailedException("队列为空!");
+
+  return _elements[_head];
 }
--- ADTs/ArrayQueue.h
+++ ADTs/ArrayQueue.h
@@ -5,25 +5,29 @@
 #ifndef DATASTRUCTURE_ALGORITHM_CPP_ARRAYQUEUE_H
 #define DATASTRUCTURE_ALGORITHM_CPP_ARRAYQUEUE_H
 
+#include "exceptions/PreconditionFailedException.h"
 #include "interfaces/Queue.h"
 
-const int MAX_CAPACITY = 10;
+const int MAX_QUEUE_CAPACITY = 10;
 
 template <typename E>
 class ArrayQueue : public Queue<E> {
  private:
-	E elements[MAX_CAPACITY];
-	int head;
-	int rear;
+  E _elements[MAX_QUEUE_CAPACITY];
+  int _head;
+  int _rear;
+  int _count;
 
  public:
+  ArrayQueue();
+
   bool isEmpty() const override;
 
   bool enqueue(const E &element) override;
 
   bool dequeue() override;
 
-  E front() const override;
+  E front() const noexcept(false) override;
 };
 
 #include "ArrayQueue.cpp"
