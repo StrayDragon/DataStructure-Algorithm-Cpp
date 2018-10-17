ade46ed93f6f175e23b70196d3c484e8e066307c
straydragon <straydragonl@foxmail.com>
2018-10-17 15:30:39 +0800

    add 5 'sort' algorithms' definition

--- Algorithms/BasicRawArraySort.cpp
+++ Algorithms/BasicRawArraySort.cpp
@@ -3,35 +3,144 @@
 //
 
 #include "BasicRawArraySort.h"
+#include <algorithm>
 
-int RawArray::SortBy::HelperFunc::findIndexOfLargest(const RawArray::ElementType *targetArray, int size) {
-	return 0;
+int RawArray::SortBy::HelperFunc::findIndexOfLargest(
+    const RawArray::ElementType *targetArray, int size) {
+  int guessBiggestIndex = 0;
+
+  for (int cur = 1; cur < size; cur++) {
+    if (targetArray[cur] > targetArray[guessBiggestIndex])
+      guessBiggestIndex = cur;
+  }
+  return guessBiggestIndex;
 }
 
-void RawArray::SortBy::selectionSort(RawArray::ElementType *targetArray, int n) {
-
+void RawArray::SortBy::selectionSort(RawArray::ElementType *targetArray,
+                                     int size) {
+  int biggestIndex;
+  for (int last = size - 1; last >= 1; last--) {
+    biggestIndex = HelperFunc::findIndexOfLargest(targetArray, last + 1);
+    std::swap(targetArray[biggestIndex], targetArray[last]);
+  }
 }
 
-void RawArray::SortBy::bubbleSort(RawArray::ElementType *targetArray, int n) {
+void RawArray::SortBy::bubbleSort(RawArray::ElementType *targetArray,
+                                  int size) {
+  bool sorted = false;
+  int pass = 1;
+  while (!sorted && (pass < size)) {
+    sorted = true;
 
+    for (int nextI, i = 0; i < size - pass; i++) {
+      nextI = i + 1;
+      if (targetArray[i] > targetArray[nextI]) {
+        std::swap(targetArray[i], targetArray[nextI]);
+        sorted = false;
+      }
+    }
+#ifdef DEBUG
+
+    for (int i = 0; i < n - pass; i++) {
+      assert(targetArray[i] < targetArray[n - pass]);
+    }
+
+#endif
+    pass++;
+  }
 }
 
-void RawArray::SortBy::insertionSort(RawArray::ElementType *targetArray, int n) {
+void RawArray::SortBy::insertionSort(RawArray::ElementType *targetArray,
+                                     int size) {
+  for (int unsorted = 0; unsorted < size; unsorted++) {
+    ElementType nextElement = targetArray[unsorted];
+    int cur = unsorted;
 
+    while ((cur > 0) && targetArray[cur - 1] > nextElement) {
+      targetArray[cur] = targetArray[cur - 1];
+      cur--;
+    }
+    targetArray[cur] = nextElement;
+  }
 }
 
-void RawArray::SortBy::HelperFunc::merge(RawArray::ElementType *targetArray, int first, int mid, int last) {
+void RawArray::SortBy::insertionSort(ElementType targetArray[], int first,
+                                     int last) {
+  for (int unsorted = first; unsorted <= last; unsorted++) {
+    ElementType nextElement = targetArray[unsorted];
+    int cur = unsorted;
 
+    while ((cur > 0) && targetArray[cur - 1] > nextElement) {
+      targetArray[cur] = targetArray[cur - 1];
+      cur--;
+    }
+    targetArray[cur] = nextElement;
+  }
 }
 
-void RawArray::SortBy::mergeSort(RawArray::ElementType *targetArray, int first, int last) {
+void RawArray::SortBy::HelperFunc::merge(RawArray::ElementType *targetArray,
+                                         int first, int mid, int last) {
+  ElementType tmpArray[RawArray::MAX_SIZE];
 
+  int first1 = first;
+  int last1 = mid;
+  int first2 = mid + 1;
+  int last2 = last;
+
+  int i = first1;
+  while ((first1 <= last1) && (first2 <= last2)) {
+    if (targetArray[first1] <= targetArray[first2]) {
+      tmpArray[i] = targetArray[first1];
+      first1++;
+    } else {
+      tmpArray[i] = targetArray[first2];
+      first2++;
+    }
+    i++;
+  }
+
+  while (first1 <= last1) {
+    tmpArray[i] = targetArray[first1];
+    first1++;
+    i++;
+  }
+
+  while (first2 <= last2) {
+    tmpArray[i] = targetArray[first2];
+    first2++;
+    i++;
+  }
+
+  for (i = first; i <= last; i++) targetArray[i] = tmpArray[i];
 }
 
-void RawArray::SortBy::quickSort(RawArray::ElementType *targetArray, int first, int last) {
-
+void RawArray::SortBy::mergeSort(RawArray::ElementType *targetArray, int first,
+                                 int last) {
+  if (first < last) {
+    int mid = first + (last - first) / 2;
+    mergeSort(targetArray, first, mid);
+    mergeSort(targetArray, mid + 1, last);
+    HelperFunc::merge(targetArray, first, mid, last);
+  }
 }
 
-void RawArray::SortBy::radixSort(RawArray::ElementType *targetArray, int n, int d) {
+int RawArray::SortBy::HelperFunc::partition(RawArray::ElementType *targetArray,
+                                            int first, int last) {
+  return (first + last) / 2;  // TODO:偷懒的分区算法
+}
 
+void RawArray::SortBy::quickSort(RawArray::ElementType *targetArray, int first,
+                                 int last) {
+  if (last - first + 1 < RawArray::MIN_SIZE) {
+    RawArray::SortBy::insertionSort(targetArray, first, last);
+  } else {
+    int pivotIndex = HelperFunc::partition(targetArray, first, last);
+    quickSort(targetArray, first, pivotIndex - 1);
+    quickSort(targetArray, pivotIndex + 1, last);
+  }
+}
+
+void RawArray::SortBy::radixSort(RawArray::ElementType *targetArray, int size,
+                                 int d) {
+  return;  // TODO:等待实现
 }
--- Algorithms/BasicRawArraySort.h
+++ Algorithms/BasicRawArraySort.h
@@ -5,26 +5,132 @@
 #ifndef DATASTRUCTURE_ALGORITHM_CPP_BASICRAWARRAYSORT_H
 #define DATASTRUCTURE_ALGORITHM_CPP_BASICRAWARRAYSORT_H
 
+/**
+ * @brief 命名空间::定义类型别名
+ * 处理原生数组类型('[]')
+ *
+ */
 namespace RawArray {
 using ElementType = int;
-extern const int MAX_SIZE;
+/**
+ * @brief 归并排序中全局变量,代表目标数组最大元素数量
+ *
+ */
+const int MAX_SIZE = 10;
+const int MIN_SIZE = 10;
+/**
+ * @brief 命名空间::排序方式
+ * 排序默认为升序
+ *
+ */
 namespace SortBy {
-void selectionSort(ElementType targetArray[], int n);
+/**
+ * @brief 选择排序
+ *
+ * @post 升序排序完成,目标数组元素数量不变
+ * @param targetArray 目标数组
+ * @param size 元素数量
+ */
+void selectionSort(ElementType targetArray[], int size);
 namespace HelperFunc {
-		int findIndexOfLargest(const ElementType targetArray[], int size);
-}
-void bubbleSort(ElementType targetArray[], int n);
+/**
+ * @brief selectionSort函数的辅助函数
+ * 返回数组中找到的最大元素的下标
+ * @pre size >= 1
+ * @post 数组内部元素状态不变
+ * @param targetArray 目标数组
+ * @param size 目标数组大小
+ * @return int 最大元素的下标
+ */
+int findIndexOfLargest(const ElementType targetArray[], int size);
+}  // namespace HelperFunc
 
-void insertionSort(ElementType targetArray[],int n);
+/**
+ * @brief 冒泡排序
+ *
+ * @post 升序排序完成,目标数组元素数量不变
+ * @param targetArray 目标数组
+ * @param size 元素数量
+ */
+void bubbleSort(ElementType targetArray[], int size);
 
+/**
+ * @brief 插入排序
+ *
+ * @post 升序排序完成,目标数组元素数量不变
+ * @param targetArray 目标数组
+ * @param size 元素数量
+ */
+void insertionSort(ElementType targetArray[], int size);
+
+/**
+ * @brief 插入排序(重载版本)
+ *
+ * @post 升序排序完成,目标数组元素数量不变
+ * @param targetArray 目标数组
+ * @param first 待排序起点
+ * @param last 待排序终点
+ */
+void insertionSort(ElementType targetArray[], int first, int last);
+
+/**
+ * @brief 归并排序
+ *
+ * @post 升序排序完成,目标数组从first到last(数组长度-1)被排序
+ * @param targetArray 目标数组
+ * @param first 待排序数组(片段)起点
+ * @param last 待排序数组(片段)终点(包含)
+ */
 void mergeSort(ElementType targetArray[], int first, int last);
 namespace HelperFunc {
+/**
+ * @brief mergeSort的辅助函数
+ *
+ * 合并两个已经排好序的数组片段为一个排好序的数组<br>
+ * 数组片段1:[first,mid]<br>
+ * 数组片段2:[mid+1,last]<br>
+ *
+ * @param targetArray 目标数组
+ * @param first 数组片段1起点
+ * @param mid 数组片段1终点,mid+1为数组片段2起点
+ * @param last 数组片段2终点
+ */
 void merge(ElementType targetArray[], int first, int mid, int last);
-}
+}  // namespace HelperFunc
+
+/**
+ * @brief 快速排序
+ *
+ * @post 升序排序完成,目标数组元素数量不变
+ * @param targetArray 目标数组
+ * @param first 目标数组起点
+ * @param last 目标数组终点
+ */
 void quickSort(ElementType targetArray[], int first, int last);
+namespace HelperFunc {
+/**
+ * @brief quickSort的辅助函数
+ *
+ * 用于将目标数分成三份 :<br>    片段1 | 基准点(pivot) | 片段2 <br>
+ * 返回基准点(数组下标值)
+ * @param targetArray 目标数组
+ * @param first 数组片段起点
+ * @param last 数组片段终点
+ * @return int 基准点(pivot)值(数组下标值)
+ */
+int partition(ElementType targetArray[], int first, int last);
+}  // namespace HelperFunc
+/**
+ * @brief 基数排序(桶排序)
+ *
+ * @post 升序排序完成,目标数组元素数量不变
+ * @param targetArray 目标数组
+ * @param size 元素数量
+ * @param d 元素位数
+ */
+void radixSort(ElementType targetArray[], int size, int d);
+}  // namespace SortBy
+}  // namespace RawArray
 
-void radixSort(ElementType targetArray[], int n, int d);
-}
-}
-
+#include "BasicRawArraySort.cpp"
 #endif  // DATASTRUCTURE_ALGORITHM_CPP_BASICRAWARRAYSORT_H
