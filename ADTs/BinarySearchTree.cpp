de2d7f603da8ca67d87ff821b0ee439b87990a65
Peilong Li <straydragonl@foxmail.com>
2018-10-22 22:28:56 +0800

    finish BinarySearchTree<E> definitions

--- ADTs/BinarySearchTree.cpp
+++ ADTs/BinarySearchTree.cpp
@@ -1,111 +1,191 @@
 //
 // Created by straydragon on 18-10-15.
 //
-// TODO:等待实现BinarySearchTree 1/2
 #include "BinarySearchTree.h"
 
 template <typename E>
 BinaryNode<E>* BinarySearchTree<E>::_insertInOrder(BinaryNode<E>* subTreePtr,
-                                                   BinaryNode<E>* newNode) {
-  return BinaryNode<E>();
+                                                   BinaryNode<E>* newNodePtr) {
+  if (subTreePtr == nullptr) {
+    return newNodePtr;
+  } else if (newNodePtr->getElement() < subTreePtr->getElement()) {
+    auto* tmp = _insertInOrder(subTreePtr->getLeft(), newNodePtr);
+    subTreePtr->setLeft(tmp);
+  } else {
+    auto* tmp = _insertInOrder(subTreePtr->getRight(), newNodePtr);
+    subTreePtr->setRight(tmp);
+  }
 }
 
 template <typename E>
-BinaryNode<E>* BinarySearchTree<E>::_removeElement(BinaryNode<E>* subTreePtr,
-                                                   const E& target,
-                                                   bool& success) {
-  return BinaryNode<E>();
+BinaryNode<E>* BinarySearchTree<E>::_removeValue(BinaryNode<E>* subTreePtr,
+                                                 const E& target,
+                                                 bool& success) {
+  if (subTreePtr == nullptr)
+    success = false;
+  else if (subTreePtr->getElement() == target) {
+    subTreePtr = _removeNode(subTreePtr);
+    success = true;
+  } else if (subTreePtr->getElement() > target) {
+    auto* tmp = _removeValue(subTreePtr->getLeft(), target, success);
+    subTreePtr->setLeft(tmp);
+  } else {
+    auto* tmp = _removeValue(subTreePtr->getRight(), target, success);
+    subTreePtr->setRight(tmp);
+  }
+  return subTreePtr;
 }
 
 template <typename E>
 BinaryNode<E>* BinarySearchTree<E>::_removeNode(BinaryNode<E>* nodePtr) {
-  return BinaryNode<E>();
+  if (nodePtr->isLeaf()) {
+    // 没儿子
+    delete nodePtr;
+    nodePtr = nullptr;
+    return nodePtr;
+  } else if (nodePtr->getLeft() && !nodePtr->getRight()) {
+    // 只有左儿子
+    auto* tmp = nodePtr->getLeft();
+    delete nodePtr;
+    nodePtr = nullptr;
+    return tmp;
+  } else if (!nodePtr->getLeft() && nodePtr->getRight()) {
+    // 只有右儿子
+    auto* tmp = nodePtr->getRight();
+    delete nodePtr;
+    nodePtr = nullptr;
+    return tmp;
+  } else {
+    // 有两个儿子
+    E properNodeElement;
+    auto getProperNodeElement = [&](E& e) -> void { properNodeElement = e; };
+    BinaryNodeTree<E>::_inorderTraverseHelper(getProperNodeElement, nodePtr);
+    auto* tmp = _removeLeftMostNode(nodePtr->getRight(), properNodeElement);
+    nodePtr->setRight(tmp);
+    nodePtr->setElement(properNodeElement);
+    return nodePtr;
+  }
 }
 
 template <typename E>
-BinaryNode<E>* BinarySearchTree<E>::_removeLeftLeafNode(
+BinaryNode<E>* BinarySearchTree<E>::_removeLeftMostNode(
     BinaryNode<E>* subTreePtr,
     E& inorderSuccessor) {
-  return BinaryNode<E>();
+  if (subTreePtr->getLeft() == nullptr) {
+    inorderSuccessor = subTreePtr->getElement();
+    return _removeNode(subTreePtr);
+  } else
+    return _removeLeftMostNode(subTreePtr->getLeft(), inorderSuccessor);
 }
 
 template <typename E>
 BinaryNode<E>* BinarySearchTree<E>::_findNode(BinaryNode<E>* treePtr,
                                               const E& target,
                                               bool& success) {
-  return BinaryNode<E>();
+  if (treePtr == nullptr)
+    return nullptr;  //没找到 QwQ
+  else if (treePtr->getElement() == target) {
+    success = true;
+    return treePtr;  //找到了 ^_^
+  } else if (treePtr->getElement() < target)
+    return _findNode(treePtr->getRight(), target,
+                     success);  //目标好像在左面 (-;
+  else
+    return _findNode(treePtr->getLeft(), target, success);  //目标好像在右面 ;-)
 }
 
 template <typename E>
-BinarySearchTree<E>::BinarySearchTree() {}
+BinarySearchTree<E>::BinarySearchTree() : BinaryNodeTree<E>() {}
 
 template <typename E>
-BinarySearchTree<E>::BinarySearchTree(const E& rootElement) {}
+BinarySearchTree<E>::BinarySearchTree(const E& rootElement)
+    : BinaryNodeTree<E>(rootElement) {}
 
 template <typename E>
 BinarySearchTree<E>::BinarySearchTree(
-    const BinarySearchTree<E>& binarySearchTree) {}
+    const BinarySearchTree<E>& binarySearchTree)
+    : BinaryNodeTree<E>(binarySearchTree) {}
 
 template <typename E>
-BinarySearchTree<E>::~BinarySearchTree() {}
+BinarySearchTree<E>::~BinarySearchTree() {
+  BinaryNodeTree<E>::_destroyTree(_rootPtr);
+}
 
 template <typename E>
 BinarySearchTree<E>& BinarySearchTree<E>::operator=(
     const BinarySearchTree<E>& binarySearchTree) {
-  return BinaryNode<E>();
+  if (this != &binarySearchTree) {
+    this->clear();
+    this = _copyTree(binarySearchTree._rootPtr);
+  }
+  return *this;
 }
 
-template <typename E>
-bool BinarySearchTree<E>::isEmpty() const {
-  return false;
-}
+// template <typename E>
+// bool BinarySearchTree<E>::isEmpty() const {
+//  return BinaryNodeTree<E>::isEmpty();
+//}
 
-template <typename E>
-int BinarySearchTree<E>::getHeight() const {
-  return 0;
-}
-
-template <typename E>
-int BinarySearchTree<E>::getNumberOfNodes() const {
-  return 0;
-}
-
-template <typename E>
-E BinarySearchTree<E>::getRootElement() const {
-  return E();
-}
-
-template <typename E>
-void BinarySearchTree<E>::setRootElement(const E& element) {}
+// template <typename E>
+// int BinarySearchTree<E>::getHeight() const {
+//  return BinaryNodeTree<E>::_getHeightHelper(_rootPtr);
+//}
+//
+// template <typename E>
+// int BinarySearchTree<E>::getNumberOfNodes() const {
+//  return BinaryNodeTree<E>::_getNumberOfNodesHelper(_rootPtr);
+//}
+//
+// template <typename E>
+// E BinarySearchTree<E>::getRootElement() const {
+//  return BinaryNodeTree<E>::getRootElement();
+//}
+//
+// template <typename E>
+// void BinarySearchTree<E>::setRootElement(const E& element) {
+//  BinaryNodeTree<E>::setRootElement(element);
+//}
 
 template <typename E>
 bool BinarySearchTree<E>::add(const E& element) {
-  return false;
+  auto* newNodePtr = new BinaryNode<E>(element);
+  _rootPtr = _insertInOrder(_rootPtr, newNodePtr);
+  return true;
 }
 
 template <typename E>
 bool BinarySearchTree<E>::remove(const E& element) {
-  return false;
+  bool removed = false;
+  _removeValue(_rootPtr, element, removed);
+  return removed;
 }
 
-template <typename E>
-void BinarySearchTree<E>::clear() {}
+// template <typename E>
+// void BinarySearchTree<E>::clear() {
+//  BinaryNodeTree<E>::clear();
+//}
+//
+// template <typename E>
+// E BinarySearchTree<E>::getElement(const E& element) const {
+//  return BinaryNodeTree<E>::getElement(element);
+//}
 
-template <typename E>
-E BinarySearchTree<E>::getElement(const E& element) const {
-  return E();
-}
+// template <typename E>
+// bool BinarySearchTree<E>::contains(const E& element) const {
+//  return BinaryNodeTree<E>::contains(element);
+//}
+//
+// template <typename E>
+// void BinarySearchTree<E>::preorderTraverse(void (*visit)(E&)) const {
+//  BinaryNodeTree<E>::preorderTraverse(visit);
+//}
 
-template <typename E>
-bool BinarySearchTree<E>::contains(const E& element) const {
-  return false;
-}
-
-template <typename E>
-void BinarySearchTree<E>::preorderTraverse(void (*visit)(E&)) const {}
-
-template <typename E>
-void BinarySearchTree<E>::inorderTraverse(void (*visit)(E&)) const {}
-
-template <typename E>
-void BinarySearchTree<E>::postorderTraverse(void (*visit)(E&)) const {}
+// template <typename E>
+// void BinarySearchTree<E>::inorderTraverse(void (*visit)(E&)) const {
+//  BinaryNodeTree<E>::inorderTraverse(visit);
+//}
+//
+// template <typename E>
+// void BinarySearchTree<E>::postorderTraverse(void (*visit)(E&)) const {
+//  BinaryNodeTree<E>::postorderTraverse(visit);
+//}
--- ADTs/BinarySearchTree.h
+++ ADTs/BinarySearchTree.h
@@ -1,7 +1,6 @@
 //
 // Created by straydragon on 18-10-15.
 //
-// TODO:等待实现BinarySearchTree 1/2
 #ifndef DATASTRUCTURE_ALGORITHM_CPP_BINARYSEARCHTREE_H
 #define DATASTRUCTURE_ALGORITHM_CPP_BINARYSEARCHTREE_H
 
@@ -17,55 +16,76 @@ class BinarySearchTree : public BinaryNodeTree<E> {
   BinaryNode<E>* _rootPtr;
 
  protected:
+  /**
+   * @brief (辅助函数) 递归的找到合适的位置,并将newNode插入进去,
+   */
   BinaryNode<E>* _insertInOrder(BinaryNode<E>* subTreePtr,
-                                BinaryNode<E>* newNode);
+                                BinaryNode<E>* newNodePtr);
 
-  BinaryNode<E>* _removeElement(BinaryNode<E>* subTreePtr,
-                                const E& target,
-                                bool& success);
+  /**
+   * @brief (辅助函数) 移除树中存在的指定元素(target)
+   * P.S.该函数遮盖掉了protected继承的同名函数
+   */
+  BinaryNode<E>* _removeValue(BinaryNode<E>* subTreePtr,
+                              const E& target,
+                              bool& success);
 
+  /**
+   * @brief (辅助函数) 从树中移除指定的节点
+   */
   BinaryNode<E>* _removeNode(BinaryNode<E>* nodePtr);
 
-  BinaryNode<E>* _removeLeftLeafNode(BinaryNode<E>* subTreePtr,
+  /**
+   * @brief (辅助函数) 从树中移除二叉搜索树中最左面的叶子(权值最低节点)
+   */
+  BinaryNode<E>* _removeLeftMostNode(BinaryNode<E>* subTreePtr,
                                      E& inorderSuccessor);
 
+  /**
+   * @brief (辅助函数)
+   * 丛二叉树的查找指定元素,若查找成功,返回该节点;否则为nullptr
+   * P.S.该函数遮盖掉了protected继承的同名函数
+   */
   BinaryNode<E>* _findNode(BinaryNode<E>* treePtr,
                            const E& target,
                            bool& success);
 
  public:
   BinarySearchTree();
-  BinarySearchTree(const E& rootElement);
+
+  explicit BinarySearchTree(const E& rootElement);
+
   BinarySearchTree(const BinarySearchTree<E>& binarySearchTree);
+
   virtual ~BinarySearchTree();
 
   BinarySearchTree<E>& operator=(const BinarySearchTree<E>& binarySearchTree);
 
-  bool isEmpty() const override;
+//  bool isEmpty() const override;
 
-  int getHeight() const override;
-
-  int getNumberOfNodes() const override;
-
-  E getRootElement() const override;
-
-  void setRootElement(const E& element) override;
+//  int getHeight() const override;
+//
+//  int getNumberOfNodes() const override;
+//
+//  E getRootElement() const override;
+//
+//  void setRootElement(const E& element) override;
 
   bool add(const E& element) override;
 
   bool remove(const E& element) override;
 
-  void clear() override;
-
-  E getElement(const E& element) const override;
-
-  bool contains(const E& element) const override;
-
-  void preorderTraverse(void (*visit)(E&)) const override;
-
-  void inorderTraverse(void (*visit)(E&)) const override;
-
-  void postorderTraverse(void (*visit)(E&)) const override;
+//  void clear() override;
+//
+//  E getElement(const E& element) const override;
+//
+//  bool contains(const E& element) const override;
+//
+//  void preorderTraverse(void (*visit)(E&)) const override;
+//
+//  void inorderTraverse(void (*visit)(E&)) const override;
+//
+//  void postorderTraverse(void (*visit)(E&)) const override;
 };
 
 #include "BinarySearchTree.cpp"
