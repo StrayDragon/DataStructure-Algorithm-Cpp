833601d2d353174d6b4f4677e3a08de4fd224b46
Peilong Li <straydragonl@foxmail.com>
2019-04-09 16:54:20 +0800

    add TriNode (not finish!) for preparing the 2-3 Tree implementation

---
+++ src/Utilities/Rc.hpp
@@ -0,0 +1,12 @@
+//
+// Created by straydragon on 19-4-9.
+//
+
+#ifndef DATASTRUCTURE_ALGORITHM_CPP_RC_H
+#define DATASTRUCTURE_ALGORITHM_CPP_RC_HPP
+
+#include <memory>
+template<typename T>
+using Rc = std::shared_ptr<T>;
+
+#endif //DATASTRUCTURE_ALGORITHM_CPP_RC_H
